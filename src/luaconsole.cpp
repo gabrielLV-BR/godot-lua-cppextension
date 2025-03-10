5f37a8ce134ed62aef620342195a1b7d14ea3a8f
Gabriel L. <gabriellovato15@gmail.com>
2025-03-09 23:13:40 -0300

    Added support for methods with arguments

--- src/luaconsole.cpp
+++ src/luaconsole.cpp
@@ -4,19 +4,23 @@
 
 using namespace godot;
 
+using VariantType = unsigned int;
+
 static const std::string CLASS_KEY = "LuaConsole";
 
 namespace Signals {
-    const char *PRINT = "print\0";
-    const char *CALLED_METHOD = "called_method\0";
-    const char *ERROR = "error\0";
+    const char *PRINT = "print";
+    const char *CALLED_METHOD = "called_method";
+    const char *ERROR = "error";
 }
 
 namespace GlobalLuaFunctions {
-    const char *PRINT = "print\0";
+    const char *PRINT = "print";
 }
 
 extern "C" {
+    #include <stdio.h>
+
     #include "lua/lua.h"
     #include "lua/lualib.h"
     #include "lua/lauxlib.h"
@@ -33,71 +37,164 @@ extern "C" {
     }
 
     static int _method_callback(lua_State *state) {
-        const char *method_name = lua_tostring(state, lua_upvalueindex(1));
+        int argument_count = lua_gettop(state);
+
+        const char *method_name_str = lua_tostring(state, lua_upvalueindex(1));
+        std::string method_name(method_name_str);
 
         lua_getfield(state, LUA_REGISTRYINDEX, CLASS_KEY.c_str());
-        auto instance = static_cast<godot::LuaConsole*>(lua_touserdata(state, -1));
+        LuaConsole *instance = static_cast<LuaConsole*>(lua_touserdata(state, -1));
 
-        instance->emit_signal(Signals::CALLED_METHOD, method_name);
+        lua_pop(state, 1);
+
+        Callable callable;
+        CallableArgumentTypeList method_arguments;
+
+        if (instance->get_callable(method_name, callable, method_arguments)) {
+            std::vector<Variant> arguments;
+
+            for (int i = 0; i < argument_count; i++) {
+                int idx = - argument_count + i;
+
+                Variant v;
+
+                switch ((Variant::Type)method_arguments[i])
+                {
+                case Variant::Type::STRING:
+                    if (lua_isstring(state, idx)) {
+                        v = String(lua_tostring(state, idx));
+                    }
+                    break;
+                case Variant::Type::INT:
+                    if (lua_isinteger(state, idx)) {
+                        v = lua_tointeger(state, idx);
+                    }
+                    break;
+                case Variant::Type::FLOAT:
+                    if (lua_isnumber(state, idx)) {
+                        #if LUA_FLOAT_TYPE == LUA_FLOAT_FLOAT
+                            v = (float) lua_tonumber(state, idx);
+                        #else
+                            v = (double) lua_tonumber(state, idx);
+                        #endif
+                    }
+                    break;
+                case Variant::Type::BOOL:
+                    if (lua_isboolean(state, idx)) {
+                        v = (bool) lua_toboolean(state, idx);
+                    }
+                    break;
+                }
+
+                if (v.get_type() == Variant::Type::NIL) {
+                    instance->emit_signal(Signals::ERROR, String("Unexpected argument type"));
+                }
+                
+                arguments.push_back(v);
+            }
+
+            if (arguments.size() != method_arguments.size()) {
+                instance->emit_signal(Signals::ERROR, String("Insufficient amount of parameters"));
+            }
+
+            for (int i = arguments.size() - 1; i >= 0; i--) {
+                callable = callable.bind(arguments[i]);
+            }
+
+            callable.call();
+        }
+            
+        instance->emit_signal(Signals::CALLED_METHOD, String(method_name_str));
 
         return 0;
     }
 }
 
 LuaConsole::LuaConsole() {
-    _lua_state = nullptr;
+    _lua = nullptr;
 }
 
 LuaConsole::~LuaConsole() {
-    if (_lua_state != nullptr)  {
-        lua_close(_lua_state);
-        _lua_state = nullptr;
+    if (_lua != nullptr)  {
+        lua_close(_lua);
+        _lua = nullptr;
     }
 }
 
-void LuaConsole::setup_lua() {
-    if (_lua_state != nullptr) {
-        lua_close(_lua_state);
+void LuaConsole::reset() {
+    print_line(String("Resetting"));
+
+    if (_lua != nullptr) {
+        lua_close(_lua);
     }
 
-    _lua_state = luaL_newstate();
+    _lua = luaL_newstate();
 
-    luaL_openlibs(_lua_state);
+    luaL_openlibs(_lua);
 
-    lua_pushcfunction(_lua_state, _print_callback);
-    lua_setglobal(_lua_state, GlobalLuaFunctions::PRINT);
+    lua_pushcfunction(_lua, _print_callback);
+    lua_setglobal(_lua, GlobalLuaFunctions::PRINT);
 
-    lua_pushlightuserdata(_lua_state, this);
-    lua_setfield(_lua_state, LUA_REGISTRYINDEX, CLASS_KEY.c_str());
+    lua_pushlightuserdata(_lua, this);
+    lua_setfield(_lua, LUA_REGISTRYINDEX, CLASS_KEY.c_str());
 
-    for (const auto &entry : _method_name_callback) {
-        const char* method_name_str = entry.c_str();
+    for (const auto &[name, callback] : _method_map) {
+        const auto* name_str = name.c_str();
 
-        lua_pushstring(_lua_state, method_name_str);
-        lua_pushcclosure(_lua_state, &_method_callback, 1);
-        lua_setglobal(_lua_state, method_name_str);
+        lua_pushstring(_lua, name_str);
+        lua_pushcclosure(_lua, &_method_callback, 1);
+        lua_setglobal(_lua, name_str);
     }
 }
 
-void LuaConsole::bind_method(String method_name) {
-    auto method_name_str = method_name.utf8();
+void LuaConsole::bind_method(Callable callable, TypedArray<int> arguments) {
+    auto method_name = std::string(callable.get_method().c_escape().utf8());
 
-    _method_name_callback.emplace(method_name_str);
+    CallableArgumentTypeList method_types;
+
+    for (int i = 0; i < arguments.size(); i++) {
+        auto arg_type = (int)arguments[i];
+
+        method_types.push_back(arg_type);
+    }
+
+    _method_map[method_name] = std::make_tuple(callable, method_types);
 }
 
 void LuaConsole::run_script(String script_source) {
-    setup_lua();
-
+    reset();
+    
     const char *source = script_source.utf8();
 
-    if (luaL_dostring(_lua_state, source) != LUA_OK) {
-        const char *error_message = lua_tostring(_lua_state, -1);
-        emit_signal(Signals::ERROR, error_message);
+    if (luaL_dostring(_lua, source) != LUA_OK) {
+        const char *error_message = lua_tostring(_lua, -1);
+        emit_signal(Signals::ERROR, String(error_message));
     }
 }
 
+bool LuaConsole::get_callable(const std::string& name, Callable& callable, CallableArgumentTypeList& argument_list) {
+    auto entry = _method_map.find(name);
+
+    if (entry == _method_map.end()) {
+        return false;
+    }
+
+    callable = std::get<0>(entry->second);
+    argument_list = std::get<1>(entry->second);
+
+    return true;
+}
+
+// Godot binding
+
 void LuaConsole::_bind_methods() {
-    ClassDB::bind_method(D_METHOD("bind_method", "method_name"), &LuaConsole::bind_method);
+    ClassDB::bind_method(
+        D_METHOD("bind_method", "method", "method_arguments"),
+        &LuaConsole::bind_method,
+        DEFVAL(Callable()),
+        DEFVAL(TypedArray<int>())
+    );
+
     ClassDB::bind_method(D_METHOD("run_script", "script_source"), &LuaConsole::run_script);
 
     ADD_SIGNAL(
--- src/luaconsole.hpp
+++ src/luaconsole.hpp
@@ -5,37 +5,39 @@
 #include <unordered_map>
 #include <unordered_set>
 
-#include <godot_cpp/classes/sprite2d.hpp>
+#include <godot_cpp/classes/node.hpp>
 
 extern "C" {
     #include "lua/lua.h"
 }
 
-namespace godot {
+using CallableArgumentTypeList = std::vector<int>;
 
-    class LuaConsole: public Node2D {
-        GDCLASS(LuaConsole, Node2D)
+namespace godot {
+    class LuaConsole: public Node {
+        GDCLASS(LuaConsole, Node)
 
         private:
 
-        lua_State* _lua_state;
-        std::unordered_set<std::string> _method_name_callback;
-
+        lua_State *_lua;
+        
         protected:
-
+        
         static void _bind_methods();
-
+        
         public:
+        std::unordered_map<std::string, std::tuple<Callable, CallableArgumentTypeList>> _method_map;
 
         LuaConsole();
         ~LuaConsole();
 
-        void bind_method(String method_name);
         void run_script(String script_source);
+        void bind_method(Callable callable, TypedArray<int> arguments);
+        void reset();
 
-        private:
+        // Unexposed methods
 
-        void setup_lua();
+        bool get_callable(const std::string& name, Callable& callable, CallableArgumentTypeList& argument_list);
     };
 
 }
