64d6be30d6bb9cb24a35059f5430b959234fa32a
Gustavo Herpich <gustavo.herpich@gmail.com>
2025-08-28 20:37:43 -0300

    refactor(lua-integration): extract conversion logic into dedicated methods and update header

--- src/variant_converter.cpp
+++ src/variant_converter.cpp
@@ -2,109 +2,83 @@
 
 using namespace godot;
 
-Variant LuaIntegration::get_from_lua(lua_State *lua, Variant::Type variant_type, int idx) {
-    switch (variant_type)
-    {
-        case Variant::Type::STRING:
-        if (lua_isstring(lua, idx)) {
+Variant LuaIntegration::lua_value_to_variant(lua_State *lua, int idx) {
+    int value_type = lua_type(lua, idx);
+
+    switch (value_type) {
+        case LUA_TSTRING:
             return String(lua_tostring(lua, idx));
-        }
-            break;
-        case Variant::Type::INT:
-        if (lua_isinteger(lua, idx)) {
-            return lua_tointeger(lua, idx);
-        }
-            break;
-        case Variant::Type::FLOAT:
-        if (lua_isnumber(lua, idx)) {
-            #if LUA_FLOAT_TYPE == LUA_FLOAT_FLOAT
-                return (float) lua_tonumber(lua, idx);
-            #else
+        case LUA_TNUMBER:
+            if (lua_isinteger(lua, idx)) {
+                return (int) lua_tointeger(lua, idx);
+            } else {
                 return (double) lua_tonumber(lua, idx);
-            #endif
-        }
-            break;
-        case Variant::Type::BOOL:
-        if (lua_isboolean(lua, idx)) {
-                return (bool) lua_toboolean(lua, idx);
             }
-            break;
-        case Variant::Type::ARRAY:
-        if (lua_istable(lua, idx)) {
+        case LUA_TBOOLEAN:
+            return (bool) lua_toboolean(lua, idx);
+        case LUA_TTABLE: {
             Array arr;
-            int abs_idx = lua_absindex(lua, idx); 
+            int abs_idx = lua_absindex(lua, idx);
             lua_pushnil(lua);
 
             while (lua_next(lua, abs_idx) != 0) {
-                int value_type = lua_type(lua, -1);
-
-                Variant element;
-                switch (value_type) {
-                    case LUA_TSTRING:
-                        element = String(lua_tostring(lua, -1));
-                        break;
-                    case LUA_TNUMBER:
-                        if (lua_isinteger(lua, -1)) {
-                            element = (int) lua_tointeger(lua, -1);
-                        } else {
-                            element = (double) lua_tonumber(lua, -1);
-                        }
-                        break;
-                    case LUA_TBOOLEAN:
-                        element = (bool) lua_toboolean(lua, -1);
-                        break;
-                    default:
-                        element = Variant();
-                        break;
-                }
-
-                arr.append(element);
-                lua_pop(lua, 1); 
+                arr.append(lua_value_to_variant(lua, -1));
+                lua_pop(lua, 1);
             }
             return arr;
         }
-        break;
+        default:
+            return Variant(); 
+    }
+}
+
+bool LuaIntegration::variant_to_lua_value(lua_State *lua, const Variant &variant) {
+    switch (variant.get_type()) {
+        case Variant::Type::STRING:
+            lua_pushstring(lua, ((String)variant).utf8());
+            break;
+        case Variant::Type::INT:
+            lua_pushinteger(lua, (int)variant);
+            break;
+        case Variant::Type::FLOAT:
+            #if LUA_FLOAT_TYPE == LUA_FLOAT_FLOAT
+                lua_pushnumber(lua, (float)variant);
+            #else
+                lua_pushnumber(lua, (double)variant);
+            #endif
+            break;
+        case Variant::Type::BOOL:
+            lua_pushboolean(lua, (bool)variant);
+            break;
+        case Variant::Type::ARRAY: {
+            Array arr = variant;
+            lua_newtable(lua);
+            for (int i = 0; i < arr.size(); i++) {
+                lua_pushinteger(lua, i + 1);
+                variant_to_lua_value(lua, arr[i]);
+                lua_settable(lua, -3);
+            }
+            break;
+        }
+        default:
+            return false; 
+    }
+    return true;
+}
+
+Variant LuaIntegration::get_from_lua(lua_State *lua, Variant::Type variant_type, int idx) {
+    switch (variant_type) {
+        case Variant::Type::STRING:
+        case Variant::Type::INT:
+        case Variant::Type::FLOAT:
+        case Variant::Type::BOOL:
+        case Variant::Type::ARRAY:
+            return lua_value_to_variant(lua, idx);
         default:
             return Variant();
     }
-    return Variant();
 }
 
 bool LuaIntegration::push_to_lua(lua_State *lua, Variant variant) {
-    Variant::Type variant_type = variant.get_type();
-
-    switch (variant_type)
-    {
-    case Variant::Type::STRING:
-        lua_pushstring(lua, ((String)variant).utf8());
-        break;
-    case Variant::Type::INT:
-        lua_pushinteger(lua, (int) variant);
-        break;
-    case Variant::Type::FLOAT:
-        #if LUA_FLOAT_TYPE == LUA_FLOAT_FLOAT
-            lua_pushnumber(lua, (float) variant);
-        #else
-            lua_pushnumber(lua, (double) variant);
-        #endif
-        break;
-    case Variant::Type::BOOL:
-        lua_pushboolean(lua, (bool) variant);
-        break;
-    case Variant::Type::ARRAY: {
-        Array arr = variant;  
-        lua_newtable(lua);    
-
-        for (int i = 0; i < arr.size(); i++) {
-            lua_pushinteger(lua, i + 1); 
-            push_to_lua(lua, arr[i]);    
-            lua_settable(lua, -3);        
-        }
-        break;
-    }
-    default:
-        return false;
-    }
-
-    return true;
+    return variant_to_lua_value(lua, variant);
 }
--- src/variant_converter.hpp
+++ src/variant_converter.hpp
@@ -2,6 +2,7 @@
 #define __variant_converter__
 
 #include <godot_cpp/classes/node.hpp>
+#include <godot_cpp/variant/variant.hpp>
 
 extern "C" {
     #include "lua/lua.h"
@@ -11,11 +12,14 @@ namespace godot {
 
     class LuaIntegration {
     public:
-
         static Variant get_from_lua(lua_State *lua, Variant::Type variant_type, int idx);
         static bool push_to_lua(lua_State *lua, Variant variant);
-    };
-};
 
+    private:
+        static Variant lua_value_to_variant(lua_State *lua, int idx);
+        static bool variant_to_lua_value(lua_State *lua, const Variant &variant);
+    };
+
+};
 
 #endif // __variant_converter__
